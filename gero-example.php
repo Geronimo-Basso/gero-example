17da843ac98bdf6e4cff97c63b1bccd4017f0d0d
Geronimo Basso <gerobasso01@gmail.com>
2022-07-01 12:11:09 -0300

    Added main class

---
+++ gero-example.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Gero Example
+Plugin URI: https://woocommerce.com/vendor/themesquad/
+Description: Gero Example is a plugin made for learning how to develop a plugin from scratch.
+Version: 1.0
+Author: Geronimo Basso
+Author URI: https://github.com/Geronimo-Basso
+*/
